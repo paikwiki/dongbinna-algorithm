ee64c21dc008363012e5ce9327d46f1fc218ce3b
paikwiki <paikwiki@gmail.com>
2020-10-19 10:28:53 +0900

    Done Ch06-04: 두 배열의 원소 교체

---
+++ codes/ch06/q03-change-elements-between-two-arrays.py
@@ -0,0 +1,38 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    q03-change-elements-between-two-arrays.py          :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: cbaek <cbaek@student.42seoul.kr>           +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2020/10/19 10:16:26 by cbaek             #+#    #+#              #
+#    Updated: 2020/10/19 10:22:05 by cbaek            ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+# 실전문제 - 두 배열의 원소 교체 P.182.
+
+n, k = map(int, input().split()) # N, K를 입력받기
+a = list(map(int, input().split())) # 배열 A의 모든 원소를 입력받기
+b = list(map(int, input().split())) # 배열 B의 모든 원소를 입력받기
+
+a.sort() # 배열 A는 오름차순 정렬 수행
+b.sort(reverse=True) # 배열 B는 내림차순 정렬 수행
+
+# 첫 번째 인덱스부터 확인하며, 두 배열의 원소를 최대 K번 비교
+for i in range(k):
+	# A의 원소가 B의 원소보다 작은 경우
+	if a[i] < b[i]:
+		# 두 원소를 교체
+		a[i], b[i] = b[i], a[i]
+	else: # A의 원소가 B의 원소보다 크거나 같을 때, 반복문을 탈출
+		break
+
+print(sum(a)) # 배열 A의 모든 원소의 합을 출력
+
+# 결과
+# $ python codes/ch06/q03-change-elements-between-two-arrays.py
+# 5 3
+# 1 2 5 4 3
+# 5 5 6 6 5
+# 26
